6ba55e4459895d53288e8161c14ea28bbf967bf2
Thiago André Cardoso Silva <33556919+thiagoandrecardoso@users.noreply.github.com>
2025-01-26 14:22:46 -0300

    Fix - tests

--- src/test/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE product
-(
-    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name     VARCHAR(255)   NOT NULL,
-    price    DECIMAL(10, 2) NOT NULL,
-    quantity INT            NOT NULL
-);
